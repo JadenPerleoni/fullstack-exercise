e502785b691c11fe8eca048d7bad58e0bfcbaef4
JadenPerleoni <jadenperleoni@gmail.com>
2022-12-27 13:30:55 -0800

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [loginInfo, setLoginInfo] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setLoginInfo((values) => ({ ...values, [name]: value }));
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Username: ${loginInfo.username} password: ${loginInfo.password}`);
+  };
+
+  return (
+    <div className="App">
+      <div className="login-form">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={loginInfo.username || ""}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={loginInfo.password || ""}
+              onChange={handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
